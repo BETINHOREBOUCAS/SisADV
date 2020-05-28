2f55bbd35f7cf72b1d60a919672205eb4222c684
BETINHOREBOUCAS <60826446+BETINHOREBOUCAS@users.noreply.github.com>
2020-05-28 14:07:17 -0300

    Update config.php

--- config.php
+++ config.php
@@ -11,11 +11,11 @@ if (ENVIRONMENT == 'development') {
     $config['dbuser'] = 'root';
     $config['dbpass'] = '';
 }else {
-    define("BASE_URL", "http://sisadv.epizy.com/");
-    $config['dbname'] = 'epiz_25867051_sisadv';
-    $config['host'] = 'sql201.epizy.com';
-    $config['dbuser'] = 'epiz_25867051';
-    $config['dbpass'] = 'nPcU2Td5SwS';
+    define("BASE_URL", "");
+    $config['dbname'] = '';
+    $config['host'] = '';
+    $config['dbuser'] = '';
+    $config['dbpass'] = '';
 }
 
 global $db;
